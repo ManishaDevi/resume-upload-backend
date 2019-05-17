ebf2bc17db41b7759a3683ae42856b4148bc282f
Manisha Devi <manishapks@gmail.com>
2019-05-18 02:18:03 +0530

    cors added

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors())
 
 require('./db')(() => {
     app.use('/', require('./app/routes'));
